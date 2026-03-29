52b45793614e5a5cd8cf95bcacad5b953964fffc
ssl-cloud <sslcloudlearning@gmail.com>
2026-03-29 09:52:46 +0530

    Project.tf modified

--- project.tf
+++ project.tf
@@ -3,3 +3,10 @@ tags = {
   managed_by  = "terraform"
   project     = "vm-deployment"
 }
+
+
+variable "address_space" {
+  description = "Address space for the virtual network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
